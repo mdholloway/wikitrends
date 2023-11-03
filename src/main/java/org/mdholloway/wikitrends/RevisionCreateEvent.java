c2a1cf3ebc99d08b5d8d09dec1d748c394c3c093
Michael Holloway <mdh@mdholloway.org>
2023-11-03 00:03:34 -0400

    Initial commit

---
+++ src/main/java/org/mdholloway/wikitrends/RevisionCreateEvent.java
@@ -0,0 +1,11 @@
+package org.mdholloway.wikitrends;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+// https://schema.wikimedia.org/repositories/primary/jsonschema/mediawiki/revision/create/2.0.0.yaml
+// TODO (someday): Fetch schema(s) and programmatically create data class(es) for Jackson
+public class RevisionCreateEvent {
+    public String database;
+    @JsonProperty("page_title") public String pageTitle;
+    @JsonProperty("page_namespace") public int pageNamespace;
+}
